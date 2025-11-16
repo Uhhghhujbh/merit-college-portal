41f0cccaba66cdf95cc00adde0c50efc10d46357
maxim010 <140669557+Uhhghhujbh@users.noreply.github.com>
2025-11-16 15:21:51 -0800

    Update auth.routes.js

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { studentLogin, parentLogin, adminLogin } from '../controllers/auth.controller.js';
+import { studentLogin, staffLogin, parentLogin, adminLogin } from '../controllers/auth.controller.js';
 
 const router = express.Router();
 
 router.post('/student/login', studentLogin);
+router.post('/staff/login', staffLogin);
 router.post('/parent/login', parentLogin);
 router.post('/admin/login', adminLogin);
 
